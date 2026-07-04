05fce91cf62ddf90c3505aac14015911f0d07264
Vivian2810 <vivian.michel@orange.fr>
2026-07-04 16:42:21 +0200

    fix(app): police

---
+++ app/pages/login.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { AuthFormField } from "@nuxt/ui";
+
+const fields = ref<AuthFormField[]>([
+  {
+    name: "email",
+    type: "text",
+    label: "Email",
+  },
+  {
+    name: "password",
+    type: "password",
+    label: "Password",
+  },
+]);
+</script>
+
+<template>
+  <UAuthForm
+    title="Login"
+    description="Enter your credentials to access your account."
+    icon="i-lucide-user"
+    :fields="fields"
+    class="mx-auto w-full max-w-sm"
+  />
+</template>
