a1238541fc82c4e3512ed36298e85d0e4497f5ab
Vivian2810 <vivian.michel@orange.fr>
2026-07-20 16:36:47 +0200

    feat(index): add link and address

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,9 +7,7 @@ const nomSite = "Cercle d'escrime de Laxou";
 
 <template>
   <div>
-    <UPageHeader
-      class="text-3xl font-bold mx-5 md:mx-0"
-    >
+    <UPageHeader class="text-3xl font-bold mx-5 md:mx-0">
       {{ nomSite }}
     </UPageHeader>
     <div
@@ -17,14 +15,25 @@ const nomSite = "Cercle d'escrime de Laxou";
     >
       <div>
         <p class="w-full">Bienvenue sur le site de notre club</p>
-        <p>Nous vous acceuillons dans notre club à partir de 6 ans</p>
+        <p>Nous vous accueillons dans notre club à partir de 6 ans.</p>
         <p>
           Nous y pratiquons l'escrime dans une ambiance conviviale et familiale,
           que vous soyez débutant ou compétiteur.
         </p>
         <p>
-          Vous pourrez y decouvrir les diciplines sportives et artistique de
-          l'escrime
+          Vous pourrez y découvrir les disciplines sportives et artistiques de
+          l'escrime.
+        </p>
+
+        <p>
+          Rendez-nous visite à la salle d'escrime de Laxou,
+          <a
+            href="https://www.google.com/maps/place/Cercle+d'escrime+de+Laxou/@48.6862635,6.1408367,17z/data=!3m1!4b1!4m6!3m5!1s0x4794a3a376d0bfcd:0x13141d7e43640ddd!8m2!3d48.68626!4d6.143417!16s%2Fg%2F11qn7cz4_2?entry=ttu&g_ep=EgoyMDI2MDcxNS4wIKXMDSoASAFQAw%3D%3D"
+            target="_blank"
+            class="text-primary hover:underline"
+          >
+            Liberté, 54520 Laxou 3 Pl. de la
+          </a>
         </p>
 
         <!-- Que vous soyez débutant, passionné ou compétiteur, le Cercle d'Escrime
